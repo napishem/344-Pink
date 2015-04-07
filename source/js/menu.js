4a95cb9511525c0a46c483dfbd12de19dbe466eb
poslavsky <mishapsv@gmail.com>
2015-04-07 07:55:07 +0300

    сбросил кэш

--- source/js/menu.js
+++
@@ -1,6 +0,0 @@
-var link = document.querySelector(".main-nav__toggler");
-var menu = document.querySelector(".main-nav__list");
-link.addEventListener("click", function(event) {
-event.preventDefault();
-menu.classList.toggle("main-nav__list--active");
-})
